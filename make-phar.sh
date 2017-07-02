bf28f520262700b8cf9b4c3697f4afa1d6dababf
vitex <vitex@kickass.vitexsoftware.cz>
2017-07-02 14:23:56 +0200

    Make phar from latest tag/release

--- make-phar.sh
+++ make-phar.sh
@@ -11,6 +11,9 @@ else
 fi
 
 cd phinx
+git fetch --tags --force
+TAG=`git tag | tail -n 1| sed ':a;N;$!ba;s/\n/ /g'`
+git checkout tags/$TAG
 composer install
 curl -LSs https://box-project.github.io/box2/installer.php | php
 php box.phar build
