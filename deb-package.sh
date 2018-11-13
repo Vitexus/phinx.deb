33783db72eea535bca77f7459a5e7993fee94e22
Vítězslav Dvořák <info@vitexsoftware.cz>
2018-11-13 17:08:04 +0100

    PHP5 support removed

--- deb-package.sh
+++ deb-package.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
+phar-composer build https://github.com/cakephp/phinx
 
-./make-phar.sh
-
-BINARY=`ls phinx/phinx-*.phar`
+BINARY=`ls phinx.phar`
 VERSION=`echo $BINARY | sed -e 's/phinx\/phinx-v//g' | sed -e 's/.phar//g'`
 echo version: $VERSION
-mv $BINARY phinx/phinx
 
 PACKAGE="php-phinx"
 REVISION=`cat debian/revison | perl -ne 'chomp; print join(".", splice(@{[split/\./,$_]}, 0, -1), map {++$_} pop @{[split/\./,$_]}), "\n";'`
 
-cd phinx
-CHANGES=`git log -n 1 | tail -n+5`
-cd ..
-
 dch -b -v $VERSION-$REVISION --package $PACKAGE $CHANGES
 
-debuild -i -us -uc -b
+dpkg-buildpackage -A -us -uc
 
 
 LASTVERSION=`cat debian/lastversion`
